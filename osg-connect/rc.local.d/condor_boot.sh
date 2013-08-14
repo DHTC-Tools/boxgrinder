4221612de938f6682fe28065bd02dd2e98478af6
Lincoln Bryant <lincolnb@hep.uchicago.edu>
2013-08-14 12:24:47 -0500

    cleaner way of grabbing internal IP

--- osg-connect/rc.local.d/condor_boot.sh
+++ osg-connect/rc.local.d/condor_boot.sh
@@ -3,7 +3,7 @@
 echo "## Attributes picked up at boot time
 TCP_FORWARDING_HOST=$(curl -s http://instance-data.ec2.internal/latest/meta-data/public-ipv4)
 PRIVATE_NETWORK_NAME=$(curl -s http://instance-data.ec2.internal/latest/meta-data/placement/availability-zone)
-PRIVATE_NETWORK_INTERFACE=$(ifconfig eth0 | grep "inet addr" | awk '{print $2}' | cut -d':' -f2)
+PRIVATE_NETWORK_INTERFACE=$(curl -s  curl http://169.254.169.254/latest/meta-data/local-ipv4)
 " > /etc/condor/config.d/99-boot.conf
 ## Set our hostname
 hostname $(curl http://instance-data.ec2.internal/latest/meta-data/public-hostname)
